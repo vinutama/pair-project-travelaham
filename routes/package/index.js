cece648f6845b57b0c7f3dc4645ef8f2f826f2f8
Cascade <mufhabu@gmail.com>
2018-11-22 14:33:35 +0700

    add routes, controllers, revised app.js

---
+++ routes/package/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const packageController = require('../../controllers/packageControllers')
+
+router.get('/',packageController)
+
+
---
+++ routes/user/index.js
@@ -0,0 +1,4 @@
+const router = require('express').Router()
+const userController = require('../../controllers/packageControllers')
+
+router.get('/',userController)
